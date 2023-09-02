eee3d573555fe43d8cc7582b7ad5b133555052e2
Camila Di Toro <camila.di-toro@tum.de>
2023-09-02 19:13:44 -0300

    Add empty actions for booking client

---
+++ client/src/main/java/ar/edu/itba/pod/grpc/bookingClient/BookingClient.java
@@ -0,0 +1,4 @@
+package ar.edu.itba.pod.grpc.bookingClient;
+
+public class BookingClient {
+}
---
+++ client/src/main/java/ar/edu/itba/pod/grpc/bookingClient/actions/BookingActionMapper.java
@@ -0,0 +1,18 @@
+package ar.edu.itba.pod.grpc.bookingClient.actions;
+
+import ar.edu.itba.pod.grpc.helpers.ActionMapper;
+import ar.edu.itba.pod.grpc.interfaces.Action;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class BookingActionMapper extends ActionMapper {
+    protected BookingActionMapper() {
+        super(Map.of(
+                "ATTRACTIONS", AttractionsAction::new,
+                "AVAILABILITY", AvailabilityAction::new,
+                "BOOK", BookAction::new,
+                "CONFIRM", ConfirmAction::new,
+                "CANCEL", CancelAction::new));
+    }
+}
