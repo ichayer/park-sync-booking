67017f1693a60cb2e4549a060a364b7e2b17299e
ichayer <ichayer@itba.edu.ar>
2023-09-02 02:00:18 -0300

    Replace LocalDate and LocalTime parser methods to return Optional instead of null

--- server/src/main/java/ar/edu/itba/pod/grpc/server/services/AdminServiceImpl.java
+++ server/src/main/java/ar/edu/itba/pod/grpc/server/services/AdminServiceImpl.java
@@ -10,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class AdminServiceImpl extends AdminServiceGrpc.AdminServiceImplBase {
 
@@ -23,32 +24,35 @@ public class AdminServiceImpl extends AdminServiceGrpc.AdminServiceImplBase {
 
     @Override
     public void addAttraction(AttractionRequest request, StreamObserver<BooleanResponse> responseObserver) {
+        boolean success = false;
         String attractionName = request.getName();
-        LocalTime openTime = parseTimeOrNull(request.getHoursFrom());
-        LocalTime closeTime = parseTimeOrNull(request.getHoursTo());
+        Optional<LocalTime> openTime = parseTimeOrNull(request.getHoursFrom());
+        Optional<LocalTime> closeTime = parseTimeOrNull(request.getHoursTo());
         int slotGap = request.getSlotGap();
 
-        boolean isValid = isValidAttractionRequest(attractionName, openTime, closeTime, slotGap);
+        if (openTime.isPresent() && closeTime.isPresent()) {
+            success = isValidAttractionRequest(attractionName, openTime.get(), closeTime.get(), slotGap);
 
-        if (isValid) {
-            Attraction attraction = new Attraction(attractionName, openTime, closeTime, slotGap);
-            attractions.put(attractionName, attraction);
+            if (success) {
+                Attraction attraction = new Attraction(attractionName, openTime.get(), closeTime.get(), slotGap);
+                attractions.put(attractionName, attraction);
+            }
         }
 
-        responseObserver.onNext(BooleanResponse.newBuilder().setSuccess(isValid).build());
+        responseObserver.onNext(BooleanResponse.newBuilder().setSuccess(success).build());
         responseObserver.onCompleted();
     }
 
     @Override
     public void addTicket(TicketRequest request, StreamObserver<BooleanResponse> responseObserver) {
         boolean success = false;
-        LocalDate date = parseDateOrNull(request.getDayOfYear());
+        Optional<LocalDate> date = parseDateOrNull(request.getDayOfYear());
 
         // TODO: Check if PassType.forNumber(request.getPassType().getNumber() != null) validation is necessary
-        if (date != null) {
+        if (date.isPresent()) {
             tickets.putIfAbsent(request.getVisitorId(), new HashMap<>());
             Map<LocalDate, PassType> visitorTickets = tickets.get(request.getVisitorId());
-            success = visitorTickets.putIfAbsent(date, request.getPassType()) == null;
+            success = visitorTickets.putIfAbsent(date.get(), request.getPassType()) == null;
         }
         responseObserver.onNext(BooleanResponse.newBuilder().setSuccess(success).build());
         responseObserver.onCompleted();
@@ -57,12 +61,12 @@ public class AdminServiceImpl extends AdminServiceGrpc.AdminServiceImplBase {
     @Override
     public void addCapacity(CapacityRequest request, StreamObserver<CapacityResponse> responseObserver) {
         String attractionName = request.getAttractionName();
-        LocalDate date = parseDateOrNull(request.getDayOfYear());
+        Optional<LocalDate> date = parseDateOrNull(request.getDayOfYear());
         int capacity = request.getCapacity();
 
-        if (attractions.containsKey(attractionName) && date != null && capacity > 0) {
+        if (attractions.containsKey(attractionName) && date.isPresent() && capacity > 0) {
             Attraction attraction = attractions.get(attractionName);
-            attraction.setCapacityByDate(date, capacity);
+            attraction.setCapacityByDate(date.get(), capacity);
         }
 
         // TODO: Confirm, cancel or assign another attraction to the visitor
@@ -87,22 +91,26 @@ public class AdminServiceImpl extends AdminServiceGrpc.AdminServiceImplBase {
             return false;
         }
 
-        return openTime != null && closeTime != null && openTime.isBefore(closeTime);
+        return openTime.isBefore(closeTime);
     }
 
-    private LocalDate parseDateOrNull(String date) {
+    private Optional<LocalDate> parseDateOrNull(String date) {
+        Optional<LocalDate> parsedDate;
         try {
-            return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            parsedDate = Optional.of(LocalDate.parse(date, DateTimeFormatter.ofPattern("dd-MM-yyyy")));
         } catch (DateTimeParseException e) {
-            return null;
+            parsedDate = Optional.empty();
         }
+        return parsedDate;
     }
 
-    private LocalTime parseTimeOrNull(String time) {
+    private Optional<LocalTime> parseTimeOrNull(String time) {
+        Optional<LocalTime> parsedTime;
         try {
-            return LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
+            parsedTime = Optional.of(LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm")));
         } catch (DateTimeParseException e) {
-            return null;
+            parsedTime = Optional.empty();
         }
+        return parsedTime;
     }
 }
