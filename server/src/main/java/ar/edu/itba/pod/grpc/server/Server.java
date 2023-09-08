48c3b24ba541ba6501a2eeda58b1dacf909f5269
ichayer <ichayer@itba.edu.ar>
2023-09-08 19:04:35 -0300

    Add adminService and bookingService in Server

--- server/src/main/java/ar/edu/itba/pod/grpc/server/Server.java
+++ server/src/main/java/ar/edu/itba/pod/grpc/server/Server.java
@@ -1,10 +1,14 @@
 package ar.edu.itba.pod.grpc.server;
 
+import ar.edu.itba.pod.grpc.server.services.AdminServiceImpl;
+import ar.edu.itba.pod.grpc.server.services.BookingServiceImpl;
+import ar.edu.itba.pod.grpc.server.services.DataHandler;
 import io.grpc.ServerBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
     private static final Logger logger = LoggerFactory.getLogger(Server.class);
@@ -12,8 +16,13 @@ public class Server {
     public static void main(String[] args) throws InterruptedException, IOException {
         logger.info("Server Starting ...");
 
+        DataHandler dataHandler = new DataHandler(new ConcurrentHashMap<>(), new ConcurrentHashMap<>());
         int port = 50051;
-        io.grpc.Server server = ServerBuilder.forPort(port).build().start();
+        io.grpc.Server server = ServerBuilder.forPort(port)
+                .addService(new AdminServiceImpl(dataHandler))
+                .addService(new BookingServiceImpl(dataHandler))
+                .build()
+                .start();
         logger.info("Server started, listening on " + port);
         server.awaitTermination();
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
