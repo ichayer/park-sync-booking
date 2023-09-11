4ef3b08416202b8d49fb73c941917a8b72c9c2fc
Camila Di Toro <camila.di-toro@tum.de>
2023-09-11 13:08:03 -0300

    Map ApiStatus codes to clients

--- client/src/main/java/ar/edu/itba/pod/grpc/GenericClient.java
+++ client/src/main/java/ar/edu/itba/pod/grpc/GenericClient.java
@@ -19,11 +19,16 @@ public class GenericClient {
     private static final Logger logger = LoggerFactory.getLogger(GenericClient.class);
     private static final Map<String, String> errorMapper = Map.ofEntries(
             Map.entry("UNKNOWN", "Unknown error."),
-            Map.entry("ATTRACTION_NOT_EXISTS", "Attraction does not exist."),
+            Map.entry("ATTRACTION_NOT_FOUND", "Attraction does not exist."),
             Map.entry("INVALID_DAY", "Invalid day."),
             Map.entry("NEGATIVE_CAPACITY", "Capacity cannot be negative."),
+            Map.entry("INVALID_DURATION", "The provided slot duration is not valid"),
+            Map.entry("INVALID_OPENING_AND_CLOSING_TIME", "The provided opening and closing times are not valid."),
             Map.entry("CAPACITY_ALREADY_DEFINED", "Capacity has already been loaded."),
+
             Map.entry("INVALID_SLOT", "Invalid slot."),
+            Map.entry("INVALID_VISITOR_ID", "The provided visitorId is not valid."),
+
             Map.entry("RESERVATION_ALREADY_EXISTS", "Reservation already exists."),
             Map.entry("MISSING_PASS", "Client does not have a valid pass."),
             Map.entry("NO_CAPACITY", "No capacity available."),
@@ -32,7 +37,10 @@ public class GenericClient {
             Map.entry("TICKET_ALREADY_EXISTS", "The user already has a ticket."),
             Map.entry("EMPTY_ATTRACTION", "Attraction name cannot be empty."),
             Map.entry("INVALID_TICKET_TYPE", "The given ticket type is invalid"),
-            Map.entry("RESERVATION_NOT_FOUND", "Reservation not found."));
+            Map.entry("RESERVATION_NOT_FOUND", "Reservation not found."),
+            Map.entry("OUT_OF_CAPACITY", "The capacity of the attraction is full."),
+            Map.entry("ALREADY_REGISTERED_FOR_NOTIFICATIONS", "The visitor is already registered for notifications of the provided day and attraction"),
+            Map.entry("NOT_REGISTERED_FOR_NOTIFICATIONS", "The visitor is not registered for notifications of the provided day and attraction"));
     ;
 
     public GenericClient(ActionMapper actionMapper){
